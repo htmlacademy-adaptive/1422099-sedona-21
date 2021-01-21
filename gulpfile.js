963b2b09424ae42bebcf09febc02d72b4bfeb755
Mikhail Suhaka <msuhaka@gmail.com>
2021-01-21 13:59:09 +0100

    Строит работающую сборку gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const svgstore = require("gulp-svgstore");
-const autoprefixer = require("autoprefixer");
+// const svgstore = require("gulp-svgstore");
+const del = require("del");
 const sync = require("browser-sync").create();
+// const uglify = require("gulp-uglify-es");
 
 
 // minHTML
@@ -22,6 +24,18 @@ const html = () => {
 }
 
 exports.htmlmin = html;
+
+// scripts
+
+// const scripts = () => {
+//   return gulp.src("source/js/script.js")
+//   .pipe(uglify())
+//   .pipe(rename("script.min.js"))
+//   .pipe(gulp.dest("build/js"))
+//   .pipe(sync.stream());
+// }
+
+// exports.scripts = scripts;
 
 // imagemin
 
@@ -49,14 +63,39 @@ exports.createWebp = createWebp;
 
 // svgSprite
 
-const sprite = () => {
-  return gulp.src("source/img/*.svg")
-  .pipe(svgstore())
-  .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("build/img"));
+// const sprite = () => {
+//   return gulp.src("source/img/*.svg")
+//   .pipe(svgstore())
+//   .pipe(rename("sprite.svg"))
+//   .pipe(gulp.dest("build/img"));
+// }
+
+// exports.sprite = sprite;
+
+// copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.{jpg,png,svg}",
+    "source/css/style.min.css"
+    ],
+    {
+    base: "source"
+    })
+    .pipe(gulp.dest("build"))
 }
 
-exports.sprite = sprite;
+exports.copy = copy;
+
+//  clean
+
+const clean = () => {
+  return del("build")
+}
+
+exports.clean = clean;
 
 // Styles
 
@@ -97,18 +136,37 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series(html, sync.reload));
 }
-
-exports.default = gulp.series(
-  styles,
-  server,
-  watcher
-);
 
 // build
 
-// const build = gulp.series {
+const build = gulp.series (
+  clean,
+  gulp.parallel (
+  styles,
+  html,
+  // sprite,
+  copy,
+  images,
+  createWebp
+  )
+)
 
-// }
+exports.build = build;
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel (
+    styles,
+    html,
+    // sprite,
+    copy,
+    createWebp
+    ),
+    gulp.series (
+      server,
+      watcher
+  )
+);
 
